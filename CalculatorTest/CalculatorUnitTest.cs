c98aaabe35f7b6c0198e363e877475120a0ae126
KLokeshAcharaya <lokesh.acharya@ics-global.in>
2022-05-10 18:23:32 +0530

    Projrct added

---
+++ CalculatorTest/CalculatorUnitTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class CalculatorUnitTest
+    {
+        [TestMethod]
+        public void Test_Addition()
+        {
+            //Arrange
+            int expected = 30;
+            int firstNumber = 20;
+            int secondNumber = 10;
+
+            //Act
+            int actual = CalculatorLibrary.Calculation.Addition(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Subtraction()
+        {
+            //Arrange
+            int expected = 10;
+            int firstNumber = 20;
+            int secondNumber = 10;
+
+            //Act
+            int actual = CalculatorLibrary.Calculation.Subtraction(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Multiplication()
+        {
+            //Arrange
+            int expected = 200;
+            int firstNumber = 20;
+            int secondNumber = 10;
+
+            //Act
+            int actual = CalculatorLibrary.Calculation.Multiplication(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Division()
+        {
+            //Arrange
+            int expected = 2;
+            int firstNumber = 20;
+            int secondNumber = 10;
+
+            //Act
+            int actual = CalculatorLibrary.Calculation.Division(firstNumber, secondNumber);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
